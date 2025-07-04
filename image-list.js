293daef259c3375e16d0cc52a2d42c15b4862143
krishan-barnwal <iamkrishan111@gmail.com>
2025-07-04 12:01:30 +0530

    image update

--- image-list.js
+++ image-list.js
@@ -4,6 +4,7 @@ const imageList = {
   bathroom: ['1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg', '6.jpeg'],
   mattress: ['1.jpeg', '2.jpeg', '3.jpeg'],
   mirror: ['1.avif', '2.avif'],
+  net: ['1.webp', '2.jpeg', '3.jpeg', '4.jpeg', '5.webp'],
 };
 
 // Function to get images for a folder
